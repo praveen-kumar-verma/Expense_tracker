a515253fc0e872f3eb82b778fe9df5ab8988e688
mr_prave <vpraveen760@gmail.com>
2022-04-07 01:41:58 +0530

    finished

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,23 +1,21 @@
-import React, { useState } from 'react'
+import React from 'react'
 
 import Card from "../UI/Card.js";
 import ExpenseDate from "./ExpenseDate.js";
 import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
-  const[title, setTitle] = useState(props.title);
-  const titleHandler = () =>{
-    setTitle('updated');
-  };
+
   return (
+    <li>
     <Card className="expense-item">
       <ExpenseDate date={props.date}/>
       <div className="expense-item__description">
-        <h2> {title} </h2>
+        <h2> {props.title} </h2>
         <div className="expense-item__price">${props.amount}</div>
-        <button onClick={titleHandler}> click me</button>
       </div>
     </Card>
+    </li>
   );
 }
 
--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 
 import "./ExpenseForm.css";
 
-const ExpenseForm = () => {
+const ExpenseForm = (props) => {
   const [enteredTitle, setEnteredTitle] = useState("");
   const [enteredAmount, setEnteredAmount] = useState("");
   const [enteredDate, setEnteredDate] = useState("");
@@ -46,14 +46,17 @@ title: enteredTitle,
 amount: enteredAmount,
 date: new Date(enteredDate),
     };
-    console.log(expenseData);
+    props.onSaveExpenseData(expenseData);
+    setEnteredTitle('');
+    setEnteredAmount('');
+    setEnteredDate('');
   };
   return (
     <form onSubmit={submitHandler}>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>Title</label>
-          <input type="text" onChange={titleChangeHandler} />
+          <input type="text" value={enteredTitle} onChange={titleChangeHandler} />
         </div>
         <div className="new-expense__control">
           <label>Amount</label>
@@ -61,6 +64,7 @@ date: new Date(enteredDate),
             type="number"
             min="0.01"
             step="0.01"
+            value={enteredAmount}
             onChange={amountChangeHandler}
           />
         </div>
@@ -70,6 +74,7 @@ date: new Date(enteredDate),
             type="date"
             min="2019-01-01"
             max="2022-12-31"
+            value={enteredDate}
             onChange={dateChangeHandler}
           />
         </div>
